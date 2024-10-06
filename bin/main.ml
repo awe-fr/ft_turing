ed692eec0d4151cb09789db42c28ea16c43229e7
AWE <rajaouisahel@gmail.com>
2024-10-06 17:29:00 +0200

    added missing json

--- bin/main.ml
+++ bin/main.ml
@@ -1,8 +1,8 @@
 open Yojson.Basic.Util
 
 let () =
-  let input = "aaababbabaa" in
-  let path = "./json/palindrome.json" in
+  let input = "00" in
+  let path = "./json/pair.json" in
   try
     let json = Check_info.check_file path input in
     Turing_machine.print_info json;
