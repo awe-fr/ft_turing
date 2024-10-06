13cf862689525c826aba5f0bea6e0e7251ca387a
AWE <rajaouisahel@gmail.com>
2024-10-06 02:29:17 +0200

    turing machine ok

--- bin/turing_machine.ml
+++ bin/turing_machine.ml
@@ -2,6 +2,21 @@ open Yojson.Basic.Util
 
 exception No_way of string
 
+let is_final json status =
+
+  let check_final status final =
+    if status = final then
+      true
+    else
+      false
+  in
+
+  let finals = json |> member "finals" |> to_list |> List.map to_string in
+  if List.exists (check_final status) finals = true then
+    true
+  else
+    false
+
 let print_info json =
   
   let print_list alphabet tp =
@@ -125,11 +140,6 @@ let print_info json =
   print_endline "----------------------------------------------------------------";
   ()
 
-
-
-
-
-
 let launch json tape =
   let rec add_blank blank tape =
     let len = String.length tape in
@@ -195,12 +205,109 @@ let launch json tape =
       end
     in
 
-    let transitions = json |> member "transitions" |> to_assoc in 
-    let next = List.assoc status transitions |> to_list in
-    let car_c = String.get tape index in
-    let car = String.make 1 car_c in
-    print_tape tape index;
-    print_trans status car next;
+    let final = is_final json status in
+
+    if final = false then
+      let transitions = json |> member "transitions" |> to_assoc in 
+      let next = List.assoc status transitions |> to_list in
+      let car_c = String.get tape index in
+      let car = String.make 1 car_c in
+      print_tape tape index;
+      print_trans status car next;
+      ()
+    else
+      ()
+  in
+
+  let rec change_state json tape index status =
+
+    let get_line json tape index status =
+
+      let rec get_line_check state car =
+        match state with
+        | [] -> raise (No_way ("Machine stuck, state : '" ^ status ^ "'."))
+        | x :: xs -> let var = x |> member "read" |> to_string in if car = var then x else get_line_check xs car
+      in
+
+      let trans = json |> member "transitions" |> to_assoc in
+      let state = List.assoc status trans |> to_list in
+      let car_c = String.get tape index in
+      let car = String.make 1 car_c in
+      let line = get_line_check state car in
+      line
+    in
+
+    let get_new_tape tape line cursor = 
+
+      let rec write_tape tape new_tape index lim =
+        if index < lim then
+          let car_c = String.get tape index in
+          let car = String.make 1 car_c in
+          let new_tape = car ^ write_tape tape new_tape (index + 1) lim in
+          new_tape
+        else
+          ""
+      in
+
+      let new_car = line |> member "write" |> to_string in
+      let tape_len = String.length tape in
+      let new_tape = write_tape tape "" 0 cursor in
+      let new_tape = new_tape ^ new_car in
+      let new_tape = new_tape ^ write_tape tape "" (cursor + 1) tape_len in
+      new_tape
+    in
+
+    let get_new_state line =
+      let new_state = line |> member "to_state" |> to_string in
+      new_state
+    in
+
+    let get_new_index line index =
+      let dir = line |> member "action" |> to_string in
+      if dir = "RIGHT" then
+        index + 1
+      else
+        index - 1
+    in
+
+    let adjust_tape json tape index =
+      let blank = json |> member "blank" |> to_string in
+      let len = String.length tape in
+      if index >= len then
+        tape ^ blank
+      else if index < 0 then
+        blank ^ tape
+      else
+        tape
+    in
+
+    let adjust_index index =
+      if index < 0 then
+        0
+      else
+        index
+    in
+
+    let print_final_tape tape =
+      print_endline "----------------------------------------------------------------";
+      Printf.printf "Tape final state : [%s]\n" tape;
+    in
+
+    let final = is_final json status in
+
+    if final = false then
+      let line = get_line json tape index status in
+      let new_tape = get_new_tape tape line index in
+      let new_status = get_new_state line in
+      let new_index = get_new_index line index in
+      let new_tape = adjust_tape json new_tape new_index in
+      let new_index = adjust_index new_index in
+      print_status json new_tape new_index new_status;
+      change_state json new_tape new_index new_status;
+      ()
+    else
+      print_final_tape tape;
+      ()
   in
 
   let index = 0 in
@@ -208,4 +315,5 @@ let launch json tape =
   let blank = json |> member "blank" |> to_string in
   let n_tape = add_blank blank tape in
   print_status json n_tape index status;
+  change_state json n_tape index status;
   ()
