de45b06ccb1050abedd7f50555908382117c97d0
AWE <rajaouisahel@gmail.com>
2024-10-06 20:44:51 +0200

    little fix

--- Makefile
+++ Makefile
@@ -5,15 +5,19 @@ INIT = opam init -y && opam install dune -y && opam install yojson -y
 
 $(TARGET):
 	$(DUNE) build
+	cp ./_build/default/bin/main.exe ./main.exe
+	mv ./main.exe ./ft_turing
 
 run: $(TARGET)
-	@$(DUNE) exec $(NAME) $(ARGS)
+	$(DUNE) exec $(NAME) $(ARGS)
+	
 
 first:
 	$(INIT)
 
 clean:
 	$(DUNE) clean
+	rm -rf ./ft_turing
 
 re: clean $(TARGET)
 
--- bin/main.ml
+++ bin/main.ml
@@ -1,23 +1,26 @@
 open Yojson.Basic.Util
 
 let () =
-  let input = "00" in
-  let path = "./json/pair.json" in
-  try
-    let json = Check_info.check_file path input in
-    Turing_machine.print_info json;
-    Turing_machine.launch json input;
+  let ac = Array.length Sys.argv in
+  if ac = 3 then
+    try
+        let json = Check_info.check_file Sys.argv.(1) Sys.argv.(2) in
+        Turing_machine.print_info json;
+        Turing_machine.launch json Sys.argv.(2);
+        ()
+    with 
+    | Type_error (msg, _) ->
+        print_endline ("Error : " ^ msg)
+    | Yojson__Common.Json_error msg ->
+        print_endline ("Error : " ^ msg)
+    | Sys_error msg ->
+        print_endline ("Error : " ^ msg)
+    | Check_info.Wrong_content msg ->
+        print_endline ("Error : " ^ msg)
+    | Turing_machine.No_way msg ->
+        print_endline ("Error : " ^ msg)
+    | _ ->
+        print_endline ("Unknown error")
+  else
+    print_endline "Wrong arguments : ./exec ./json tape";
     ()
-  with 
-  | Type_error (msg, _) ->
-      print_endline ("Error : " ^ msg)
-  | Yojson__Common.Json_error msg ->
-      print_endline ("Error : " ^ msg)
-  | Sys_error msg ->
-      print_endline ("Error : " ^ msg)
-  | Check_info.Wrong_content msg ->
-      print_endline ("Error : " ^ msg)
-  | Turing_machine.No_way msg ->
-      print_endline ("Error : " ^ msg)
-  | _ ->
-      print_endline ("Unknown error")
