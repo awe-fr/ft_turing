c7a79fc544e88a098d79e2b155f60a0db2717fcf
awe-fr <rajaouisahel@gmail.com>
2024-10-07 17:32:13 +0200

    fix help

--- bin/main.ml
+++ bin/main.ml
@@ -14,24 +14,30 @@ let () =
   in
   let ac = Array.length Sys.argv in
   if ac = 3 then
-    try
+    let cmd = Sys.argv.(1) in
+    if cmd = "--help" || cmd = "-h" then begin
+      help ();
+      ()
+    end else begin
+      try
         let json = Check_info.check_file Sys.argv.(1) Sys.argv.(2) in
         Turing_machine.print_info json;
         Turing_machine.launch json Sys.argv.(2);
         ()
-    with 
-    | Type_error (msg, _) ->
-        print_endline ("Error : " ^ msg)
-    | Yojson__Common.Json_error msg ->
-        print_endline ("Error : " ^ msg)
-    | Sys_error msg ->
-        print_endline ("Error : " ^ msg)
-    | Check_info.Wrong_content msg ->
-        print_endline ("Error : " ^ msg)
-    | Turing_machine.No_way msg ->
-        print_endline ("Error : " ^ msg)
-    | _ ->
-        print_endline ("Unknown error")
+      with 
+      | Type_error (msg, _) ->
+          print_endline ("Error : " ^ msg)
+      | Yojson__Common.Json_error msg ->
+          print_endline ("Error : " ^ msg)
+      | Sys_error msg ->
+          print_endline ("Error : " ^ msg)
+      | Check_info.Wrong_content msg ->
+          print_endline ("Error : " ^ msg)
+      | Turing_machine.No_way msg ->
+          print_endline ("Error : " ^ msg)
+      | _ ->
+          print_endline ("Unknown error")
+    end
   else if ac = 2 then
     let cmd = Sys.argv.(1) in
     if cmd = "--help" || cmd = "-h" then begin
